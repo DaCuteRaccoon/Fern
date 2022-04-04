7f1898ec925427247de559fa560900964e03440e
DaCuteRaccoon stands with Ukraine <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-04-04 12:47:21 -0700

    Update functions.rb

--- ruby/functions.rb
+++ ruby/functions.rb
@@ -1,1 +1,8 @@
+#!/usr/bin/env ruby
 
+def clear
+  Gem.win_platform? ? (system "cls") : (system "clear")
+end
+
+# https://www.google.com/search?q=how+to+require+a+file+in+ruby
+# https://stackoverflow.com/questions/16856243/how-to-require-a-ruby-file-from-another-directory
